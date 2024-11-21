b98a58a3780e08587c621892247913e9e958e77b
Iverson Zhou <zzho044@aucklanduni.ac.nz>
2024-11-21 14:20:16 +0800

    update

---
+++ perform_hyperparameter_tuning_azure_machine_learning_pipelines/use-sweep-job-hyperparameter-tuning.py
@@ -0,0 +1,74 @@
+#Create a training script for hyperparameter tuning
+import argparse
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import mlflow
+
+# get regularization hyperparameter
+parser = argparse.ArgumentParser()
+parser.add_argument('--regularization', type=float, dest='reg_rate', default=0.01)
+args = parser.parse_args()
+reg = args.reg_rate
+
+# load the training dataset
+data = pd.read_csv("data.csv")
+
+# separate features and labels, and split for training/validatiom
+X = data[['feature1','feature2','feature3','feature4']].values
+y = data['label'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+
+# train a logistic regression model with the reg hyperparameter
+model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
+
+# calculate and log accuracy
+y_hat = model.predict(X_test)
+acc = np.average(y_hat == y_test)
+mlflow.log_metric("Accuracy", acc)
+
+
+#Configure and run a sweep job
+#create a base command job 
+
+from azure.ai.ml import command
+job = command(
+    code="./src",
+    command="python train.py --regularization ${{inputs.reg_rate}}",
+    inputs={
+        "reg_rate": 0.01,
+    },
+    environment="AzureML-sklearn-0.24-ubuntu18.04-py37-cpu@latest",
+    compute="aml-cluster",
+    )
+
+
+
+#syntax to override input parameters with search space
+from azure.ai.ml.sweep import Choice
+command_job_for_sweep = job(
+    reg_rate=Choice(values=[0.01, 0.1, 1]),
+)
+
+
+
+#call command job to sweep
+from azure.ai.ml import MLClient
+
+# apply the sweep parameter to obtain the sweep_job
+sweep_job = command_job_for_sweep.sweep(
+    compute="aml-cluster",
+    sampling_algorithm="grid",
+    primary_metric="Accuracy",
+    goal="Maximize",
+)
+
+# set the name of the sweep job experiment
+sweep_job.experiment_name="sweep-example"
+
+# define the limits for this sweep
+sweep_job.set_limits(max_total_trials=4, max_concurrent_trials=2, timeout=7200)
+
+# submit the sweep
+returned_sweep_job = ml_client.create_or_update(sweep_job)
